9651023ac1fbf0841669833dc8142d24b25b9f52
Veronique Majo <vmajo02@gmail.com>
2019-06-07 15:59:11 +0200

    7 juin  Product category repository, controller ans views

---
+++ Webshop/WebShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webShop.core.Models;
+using WebShop.DataAcces.InMemory.Repositories;
+
+namespace WebShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        public ProductCategoryManagerController()
+        {
+            context = new CategoryRepository();
+        }
+        CategoryRepository context;
+        public ActionResult Index()
+        {
+            List<ProductCategory> categories = context.GetCategory().ToList();
+
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ProductCategory category = new ProductCategory();
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Create(ProductCategory ProductCategory)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(ProductCategory);
+            }
+            else
+            {
+                context.Insert(ProductCategory);
+                context.Commit();  // cette methode que nous avons crée nous permet de sauver le produit dans le cache. 
+            }
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult Delete(string Id)
+        {
+            var CategorieToDelete = context.FindCategory(Id);
+            return View(CategorieToDelete);
+        }
+        [HttpDelete]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            var CategorieToDelete = context.Delete(Id);
+            if (CategorieToDelete == false)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpGet]
+        public ActionResult Edit(string ID)
+        {
+            var CategorieToUpdate = context.FindCategory(ID);
+            return View(CategorieToUpdate);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+
+        public ActionResult Editnew(ProductCategory ProductCategory)
+        {
+            context.Update(ProductCategory);
+            //context.Delete(product.Id);
+            //context.Insert(product);
+            context.Commit();
+            return RedirectToAction("Index"); ;
+        }
+    }
+}
---
+++ Webshop/webShop.core/Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webShop.core.Models
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+        public string Category { get; set; }
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
