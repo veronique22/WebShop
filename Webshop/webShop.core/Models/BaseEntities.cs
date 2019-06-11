031793f0fd7a2c6acdec91af12225d139dd32029
Veronique Majo <vmajo02@gmail.com>
2019-06-11 13:05:25 +0200

    Base entitie classe created

---
+++ Webshop/webShop.core/Models/BaseEntities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webShop.core.Models
+{
+   public abstract class BaseEntities
+    {
+        public string Id { get; set; }
+
+        public DateTime CreateAt { get; set; }   
+        // ces deux propriétes ici nous permettent d'enregistrer par exemple quand est ce que nos objets sont crées. 
+        //public DateTime UpdateAt { get; set; }
+
+
+        public BaseEntities()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreateAt = DateTime.Now;
+           // this.UpdateAt = DateTime.Now;
+        }
+    }
+}
