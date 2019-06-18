3c426f8e22e3398394268b025d168549743af2fd
Veronique Majo <vmajo02@gmail.com>
2019-06-18 11:59:31 +0200

    Create Basket en BasketItem

---
+++ Webshop/webShop.core/Models/BasketItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webShop.core.Models
+{
+    public class BasketItem : BaseEntities
+    {
+        public string BasketID { get; set; }
+        public string ProductID { get; set; }  // il est préférable ici de garder uniquement l'id du produit et pas le produit entier
+       // ceci facilitera l'entretien. si jamais un produit est changé à la base, tout sera facilement et automatiquement mis à jour dans le panier
+
+        public int Quantity { get; set; }
+
+    }
+}
